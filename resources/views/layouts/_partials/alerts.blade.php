a4293ceea13ee5b998a17c206885a46cb465c0a1
Amaia Galvez <amaia@izt.coop>
2020-10-28 00:27:14 +0100

    alertak itxi

--- resources/views/layouts/_partials/alerts.blade.php
+++ resources/views/layouts/_partials/alerts.blade.php
@@ -1,13 +1,19 @@
 @if(Session::has('successMessage'))
 
-    <div class="alert alert-success">
+    <div class="alert alert-success" role="alert">
         {{Session::get('successMessage')}}
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
     </div>
 
 @elseif(Session::has('errorMessage'))
 
-    <div class="alert alert-danger">
+    <div class="alert alert-danger" role="alert">
         <p>{!! Session::get('errorMessage') !!}</p>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
     </div>
 
 @endif
